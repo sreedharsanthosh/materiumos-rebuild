6baaba526929795776426e64ded45c557d401c87
sreedharsanthosh <sreedharsanthosh2005@gmail.com>
2021-12-12 22:14:13 +0530

    added slider animation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 gsap.to(".home-badge", { x: 0, opacity: 1, duration: 1 });
 gsap.to(".home-text", { x: 0, opacity: 1, duration: 1 });
 gsap.to(".home-img", { x: 0, opacity: 1, duration: 1, delay: 0.3 });
+gsap.to(".slider", { y: -1000, opacity: 0, duration: 2, delay: 2.5 });
 const hamburger = document.querySelector(".hamburger");
 const list = document.querySelectorAll(".overlay-nav li");
 const navLinks = document.querySelector(".nav-overlay");
